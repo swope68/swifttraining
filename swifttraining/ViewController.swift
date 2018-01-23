0b9844e7b4564403be798de45c1874372214a0a2
Todd Swope <swope68@gmail.com>
2018-01-23 09:46:30 -0600

    Initial Commit

---
+++ swifttraining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swifttraining
+//
+//  Created by Todd Swope on 1/23/18.
+//  Copyright © 2018 Todd Swope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
